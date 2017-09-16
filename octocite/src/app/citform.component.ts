45f86501cc1998f05fecba04b4432d03af581419
tr-io <leoliu.mailbox@gmail.com>
2017-09-16 14:14:14 -0400

    Added stuff

--- octocite/src/app/citform.component.ts
+++ octocite/src/app/citform.component.ts
@@ -4,4 +4,28 @@ import { Component } from '@angular/core';
   selector: 'citation-form',
   templateUrl: './citform.component.html'
 })
-export class CitFormComponent {}
+export class CitFormComponent {
+  submitted = false;
+  
+  typeString = 1;
+  authorFirstName = '';
+  authorLastName = '';
+  sourceTitle = '';
+  sourceContainer = '';
+  version = '';
+  number = '';
+  publisher = '';
+  pubDate = '';
+  pubLoc = '';
+  sourceURL = '';
+  isbn = '';
+  pages = '';
+  
+  onSubmit() {
+    this.submitted = true;
+  }
+  
+  retry() {
+    this.submitted = false;
+  }
+}
